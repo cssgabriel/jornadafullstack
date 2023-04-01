66c106599868ff96eb81ba8158690af5b07dc776
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2023-03-31 22:25:07 -0300

    adicionando footer e sidebar

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
+        <Video
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Paulo"
+          description="Brecker: o goleiro"
+          music="Música épica"
+          url="https://privaty.com.br/wp-content/uploads/2023/03/gato-1.mp4"
+        />
+        <Video
+          likes={111}
+          messages={222}
+          shares={333}
+          name="cssGabriel"
+          description="jornadadev fullstack"
+          music="Música eletronica"
+          src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
       </div>
     </div>
   );
